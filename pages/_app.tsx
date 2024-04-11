4bf281e1e02072bc8282f9d68f94b2546607e2c7
Thinqat1985731 <kata.shin.2001@gmail.com>
2024-04-11 13:49:56 +0900

    update: using katex (only block) / top page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import "katex/dist/katex.min.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import "nextra-theme-blog/style.css";
@@ -20,6 +21,25 @@ export default function App({ Component, pageProps }: AppProps) {
           type="font/woff2"
           crossOrigin="anonymous"
         />
+        <link
+          rel="preload"
+          href="/fonts/Inter-roman.latin.var.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <script defer
+          src="https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.js"
+          integrity="sha384-cpW21h6RZv/phavutF+AuVYrr+dA8xD9zs6FwLpaCct6O9ctzYFfFr4dgmgccOTx"
+          crossOrigin="anonymous">
+        </script>
+        <script defer
+          src="https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/contrib/auto-render.min.js"
+          integrity="sha384-kWPLUVMOks5AQFrykwIup5lo0m3iMkkHrD0uJ4H5cjeGihAutqP0yW0J6dpFiVkI"
+          crossOrigin="anonymous" 
+          onLoad="renderMathInElement(document.body);"
+        >
+        </script>
       </Head>
       <Component {...pageProps} />
     </>
