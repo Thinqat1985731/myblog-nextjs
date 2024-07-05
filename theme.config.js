69968a874745e62302dd67bdf8477bf137384e9a
Thinqat1985731 <kata.shin.2001@gmail.com>
2024-07-06 00:43:10 +0900

    change: react-icons -> react-simple-icons (for adding huggingface icon)

--- theme.config.js
+++ theme.config.js
@@ -1,13 +1,14 @@
 const YEAR = new Date().getFullYear();
-import { ImGithub, ImRss } from "react-icons/im";
+import { SiGithub, SiHuggingface, SiRss } from '@icons-pack/react-simple-icons';
 
 export default {
   footer: (
     <footer>
       <small>
         <time>{YEAR}</time> © Thinqat
-        <a href="https://github.com/Thinqat1985731"><ImGithub size="25px"/></a>
-        <a href="/feed.xml"><ImRss size="25px"/></a>
+        <a href="https://huggingface.co/Thinqat1985731"><SiHuggingface size="27px"/></a>
+        <a href="https://github.com/Thinqat1985731"><SiGithub size="25px"/></a>
+        <a href="/feed.xml"><SiRss size="25px"/></a>
       </small>
       <style jsx>{`
         footer {
